8ab26451adb6d623b8d7158b173fbf3f660b857b
amberjackman <awakekaw166@protonmail.ch>
2022-12-20 18:19:52 +0900

    end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import logo from './logo.svg';
-import './App.css';
+import root from 'react-shadow'
+
+
+const styles = `
+.App {
+  text-align: center;
+}
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+p {
+  color: yellow;
+}
+`;
 
 function App() {
   return (
-    <div className="App">
+    <root.div>
+       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +66,9 @@ function App() {
         </a>
       </header>
     </div>
+    <style type="text/css"> {styles} </style>
+  </root.div>
+   
   );
 }
 
